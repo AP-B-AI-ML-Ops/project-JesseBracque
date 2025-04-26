686ebf811d76ae8171e0f60e23b5af3d32029a54
jesse bracqué <s138341@ap.be>
2025-04-26 18:26:20 +0200

    Applied principles of the second lab

---
+++ train-and-deploy/scripts/preprocess_data.py
@@ -0,0 +1,89 @@
+import os
+import pickle
+import click
+import pandas as pd
+
+from sklearn.feature_extraction import DictVectorizer
+
+
+def dump_pickle(obj, filename: str):
+    with open(filename, "wb") as f_out:
+        return pickle.dump(obj, f_out)
+    
+
+def split_and_read_data(path):
+    df = pd.read_csv(path)
+
+    for col in df.columns:
+        if col != "Date":
+            df[col] = df[col].str.replace(",", "", regex=False)
+            df[col] = pd.to_numeric(df[col], errors='coerce')
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    total_len = len(df)
+    train_end = int(0.7 * total_len)
+    val_end = int(0.85 * total_len)
+    
+    train_df = df.iloc[:train_end].sort_values("Date")
+    val_df = df.iloc[train_end:val_end].sort_values("Date")
+    test_df = df.iloc[val_end:].sort_values("Date")
+
+    return prepare_gold_data(train_df), prepare_gold_data(val_df), prepare_gold_data(test_df)
+
+
+def prepare_gold_data(df, currency_col='EUR', new_col_name='gold_diff'):
+    df = df.copy()
+    df[new_col_name] = df[currency_col].diff().fillna(0)
+    return df[['Date', currency_col, new_col_name]]
+
+
+def prepare_regression_train_or_val_data(df, dv, train):
+    df['Date'] = df['Date'].astype(str)
+    categorical = ['Date']
+    numerical = ['EUR']
+
+    data_dict = df[categorical + numerical].to_dict(orient='records')
+    Y_data = df['gold_diff'].values
+
+    if train:
+        X_data = dv.fit_transform(data_dict)
+    else:
+        X_data = dv.transform(data_dict)
+
+    return X_data, Y_data
+
+@click.command() #moeten VOOR uit te voeren functie in main!!!!
+@click.option(
+    "--raw_data_path",
+    default="./data-files",
+    help="Location where the raw NYC taxi trip data was saved"
+)
+@click.option(
+    "--dest_path",
+    default="./output",
+    help="Location where the resulting files will be saved"
+)
+def run_data_prep(raw_data_path: str = "data-files", dest_path: str = "output", dataset: str = "Daily.csv"):
+    # Load parquet files
+    df_train, df_val, df_test = split_and_read_data(
+        os.path.join(raw_data_path, f"{dataset}")
+    )
+
+    # Extract the target, fit DictVectorizer and preprocess data
+    dv = DictVectorizer()
+    X_train, y_train = prepare_regression_train_or_val_data(df_train, dv, True)
+    X_val, y_val = prepare_regression_train_or_val_data(df_val, dv, False)
+    X_test, y_test = prepare_regression_train_or_val_data(df_test, dv, False)
+
+    # Create dest_path folder unless it already exists
+    os.makedirs(dest_path, exist_ok=True)
+
+    # Save DictVectorizer and datasets
+    dump_pickle(dv, os.path.join(dest_path, "dv.pkl"))
+    dump_pickle((X_train, y_train), os.path.join(dest_path, "train.pkl"))
+    dump_pickle((X_val, y_val), os.path.join(dest_path, "val.pkl"))
+    dump_pickle((X_test, y_test), os.path.join(dest_path, "test.pkl"))
+
+
+if __name__ == '__main__':
+    run_data_prep()
---
+++ train-and-deploy/scripts/train.py
@@ -0,0 +1,62 @@
+import os
+import pickle
+import click
+
+import mlflow
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import root_mean_squared_error
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+    
+
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+def run_train(data_path: str):
+    # set the tracking uri for mlflow
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+
+    # set the experiment for mlflow
+    mlflow.set_experiment("gold-values")
+
+    # start an mlflow run
+    with mlflow.start_run():
+        # set some mlflow tags (e.g. developer)
+        mlflow.set_tag("developer", "Jesse") #laatste twee zijn afkortng eigen naam
+
+        # log params in mlflow (e.g. path to the data for validation and training data)
+        mlflow.log_param("train-data-path", "./data/green_tripdata_2021-01.parquet")
+        mlflow.log_param("valid-data-path", "./data/green_tripdata_2021-02.parquet")
+
+        # Load training and validation data
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+        # move the values for the regressor (below) to a variable
+        max_depth = 10
+        random_state = 0
+
+        # log the values for the regressor in mlflow
+        mlflow.log_param("max_depth", max_depth)
+        mlflow.log_param("random_state", random_state)
+    
+        # Train the model
+        rf = RandomForestRegressor(max_depth=max_depth, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = root_mean_squared_error(y_val, y_pred)
+
+        # log the rmse in mlflow
+        mlflow.log_metric("rmse", rmse)
+
+
+if __name__ == '__main__':
+    run_train()
